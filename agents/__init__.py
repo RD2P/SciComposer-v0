c1b0db3b0b65689c7751ee899a124a6f07275403
Raphael <gld141@mail.usask.ca>
2026-08-15 22:13:42 -0600

    remove workflow retriever agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,13 +1,11 @@
 from .builder import BuilderAgent
 from .planner import PlannerAgent
-from .tool_retriever import ToolRetrieverAgent
+from .retriever import RetrieverAgent
 from .validator import ValidatorAgent
-from .workflow_retriever import WorkflowRetrieverAgent
 
 __all__ = [
-    "BuilderAgent",
     "PlannerAgent",
-    "ToolRetrieverAgent",
+    "RetrieverAgent",
+    "BuilderAgent",
     "ValidatorAgent",
-    "WorkflowRetrieverAgent",
 ]
