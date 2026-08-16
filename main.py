ba9306e004685cdcb97bf8cf2cd058a346bb6a20
Raphael <gld141@mail.usask.ca>
2026-08-15 22:10:13 -0600

    add guard against irrelevant queries

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from graph.workflow_graph import build_workflow_graph
+from guard.guard import check_query
+
 
 def main() -> None:
     app = build_workflow_graph()
 
     while True:
         query = input("\n\nEnter your query (or 'exit' to quit): ")
-        
-        if query.lower() == 'exit':
+
+        if query == "":
+            continue
+
+        if query.lower() == "exit":
             break
 
+        query_check = check_query(query)
+
+        if query_check == "reject":
+            print("Query is not related to a Galaxy scientific workflow.")
+            continue
+
         result = app.invoke({"user_request": query})
-        print(result)        
+        print("RESULT:\n\n", result)
 
 
 if __name__ == "__main__":
